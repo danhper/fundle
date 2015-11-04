82118b79c85c69cc8f8771549d603d2e5418b593
Daniel Perez <daniel@claudetech.com>
2015-11-04 15:24:01 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,89 @@
+# fundle
+
+A minimalist package manager for [fish](http://fishshell.com/) inspired by [Vundle](https://github.com/VundleVim/Vundle.vim).
+
+All plugins are installed/updated using git, so the only requirement is to have
+git installed and on the path (and well, fish, obviously).
+
+## Installation
+
+Just drop [fundle.fish](fundle.fish) in your `~/.config/fish/functions` directory and you are done.
+
+```
+wget https://raw.githubusercontent.com/tuvistavie/fundle/master/fundle.fish -O ~/.config/fish/functions/fundle.fish
+```
+
+## Usage
+
+To add a plugin, you simply need to add
+
+```
+fundle plugin 'repo_owner/repo_name'
+```
+
+somewhere in your configuration.
+
+For example:
+
+```
+fundle plugin 'tuvistavie/fastdir'
+```
+
+will install the repository at https://github.com/tuvistavie/fastdir.
+
+If you need to change the repository, you can pass it as a second argument and
+it will be passed directly to `git clone`:
+
+```
+fundle plugin 'tuvistavie/fastdir' 'git@github.com:tuvistavie/fastdir.git'
+```
+
+After having called `fundle plugin`, you need to call
+
+```
+fundle init
+```
+
+for the plugins to be loaded.
+
+When you add new plugins, you need to run
+
+```
+fundle install
+```
+
+for fundle to download them, and to re-run `fundle init` or to restart your shell.
+
+## Plugin structure
+
+A plugin basically has the following structure.
+
+```
+.
+├── completions
+│   └── my_command.fish
+├── functions
+│   ├── __plugin_namespace_my_function.fish
+│   └── my_public_function.fish
+├── init.fish
+└── README.md
+```
+
+* `init.fish` will be sourced directly, so it should not do anything that takes too long
+  to avoid slowing down the shell startup. It is a good place to put aliases, for example.
+* `functions` is the directory containing the plugin functions. This directory will
+  be added to `fish_function_path`, and will therefore be auto loaded. I suggest you
+  prefix your functions with `__plugin_name` if the user will not be using them explicitly.
+* `completions` is the directory containing the plugin completions. This directory will
+  be added to `fish_complete_path`.
+
+## Contributing
+
+Contributions are very appreciated. Please open an issue or create a PR if you
+want to contribute.
+If you created a package compatible with fundle, feel free to [add it to the Wiki](https://github.com/tuvistavie/fundle/wiki/Home/_edit).
+
+## Motivations
+
+I know that [oh-my-fish](https://github.com/oh-my-fish/oh-my-fish) has a utility to
+install packages, but I wanted a simple tool, not a whole framework.
---
+++ fundle.fish
@@ -0,0 +1,165 @@
+function __fundle_plugins_dir -d "returns fundle directory"
+	if test -z "$fundle_plugins_dir"
+		echo $HOME/.config/fish/fundle
+	else
+		echo $fundle_plugins_dir
+	end
+end
+
+function __fundle_no_git -d "check if git is installed"
+	if not which git > /dev/null
+		echo "git needs to be installed and in the path"
+		return 0
+	end
+	return 1
+end
+
+function __fundle_get_url -d "returns the url for the given plugin"
+	echo "https://github.com/"$argv[1]".git"
+end
+
+function __fundle_update_plugin -d "update the given plugin"
+	cd $argv[1]
+	git remote set-url origin $argv[2]
+	git pull --rebase origin master
+	cd -
+end
+
+function __fundle_download_plugin -d "download the given plugin"
+	if __fundle_no_git
+		return 1
+	end
+
+	set -l plugin_dir (__fundle_plugins_dir)/$argv[1]
+	set -l git_url $argv[2]
+	set -l upgrade ""
+
+	if begin; contains -- -u $argv; or contains -- --upgrade $argv; end
+		set upgrade true
+	end
+
+	if test -d $plugin_dir
+		if test -n "$upgrade"
+			__fundle_update_plugin $plugin_dir $git_url
+		else
+			echo "$argv[1] installed in $plugin_dir"
+		end
+	else
+		git clone $git_url $plugin_dir
+	end
+end
+
+function __fundle_show_doc_msg -d "show a link to fundle docs"
+	if test (count $argv) -ge 1
+		echo $argv
+	end
+	echo "See the docs for more info. https://github.com/tuvistavie/fundle"
+end
+
+function __fundle_check_dir -d "check if fundle dir exists and warn if not"
+	set -l fundle_dir (__fundle_plugins_dir)
+	if not test -d $fundle_dir
+		__fundle_show_doc_msg "$fundle_dir is not a directory. You probably need to run 'fundle install'."
+	end
+end
+
+function __fundle_plugin_path -d "get the path in the given plugin"
+	set -l suffix ""
+	if test (count $argv) -eq 2
+		set suffix "/$argv[2]"
+	end
+	echo (__fundle_plugins_dir)/$argv[1]$suffix
+end
+
+function __fundle_init -d "initialize fundle"
+	set -l fundle_dir (__fundle_plugins_dir)
+
+	if test (count $__fundle_plugin_names) -eq 0
+		__fundle_show_doc_msg "No plugin registered. You need to call 'fundle plugin NAME' before using 'fundle init'"
+		return 1
+	end
+
+	for plugin in $__fundle_plugin_names
+		if not test -d (__fundle_plugin_path $plugin)
+			__fundle_show_doc_msg "$plugin not installed. You may need to run 'fundle install'"
+			continue
+		end
+
+		set -l init_file (__fundle_plugin_path $plugin "init.fish")
+		set -l functions_dir (__fundle_plugin_path $plugin "functions")
+		set -l completions_dir (__fundle_plugin_path $plugin "completions")
+
+		if test -f $init_file
+			source $init_file
+		end
+
+		if begin; test -d $functions_dir; and not contains $functions_dir $fish_function_path; end
+			set fish_function_path $functions_dir $fish_function_path
+		end
+
+		if begin; test -d $completions_dir; and not contains $completions_dir $fish_complete_path; end
+			set fish_complete_path $dir $fish_complete_path
+		end
+	end
+end
+
+function __fundle_install -d "install plugin"
+	if test (count $__fundle_plugin_names) -eq 0
+		__fundle_show_doc_msg "No plugin registered. You need to call 'fundle plugin NAME' before using 'fundle install'"
+	end
+
+	for i in (seq (count $__fundle_plugin_names))
+		__fundle_download_plugin $__fundle_plugin_names[$i] $__fundle_plugin_urls[$i] $argv
+	end
+end
+
+function __fundle_plugin -d "add plugin to fundle"
+	set -l plugin_url ""
+	switch (count $argv)
+		case 0
+			echo "plugin needs at least one parameter."
+			return 1
+		case 1
+			set plugin_url (__fundle_get_url $argv[1])
+		case 2
+			set plugin_url $argv[2]
+	end
+
+	if not contains $argv[1] $__fundle_plugin_names
+		set -g __fundle_plugin_names $__fundle_plugin_names $argv[1]
+		set -g __fundle_plugin_urls $__fundle_plugin_urls $plugin_url
+	end
+end
+
+function __fundle_print_help -d "prints fundle help"
+	echo "usage: fundle (init | plugin | install | help)"
+end
+
+function fundle -d "run fundle"
+	if __fundle_no_git
+		return 1
+	end
+
+	set -l sub_args ""
+
+	switch (count $argv)
+		case 0
+			__fundle_print_help
+			return 1
+		case 1
+		case '*'
+			set sub_args $argv[2..-1]
+	end
+
+	switch $argv[1]
+		case "init"
+			__fundle_init $sub_args
+		case "plugin"
+			__fundle_plugin $sub_args
+		case "install"
+			__fundle_install $sub_args
+		case "help" -h --help
+			__fundle_print_help
+			return 0
+	end
+end
