11a282a4fb9a9a973f43493639ac674087c13034
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2022-10-28 17:21:47 -0500

    Update completions

--- completions/fundle.fish
+++ completions/fundle.fish
@@ -10,11 +10,8 @@ complete -f -c fundle -n '__fish_prog_needs_command' -a self-update -d "update f
 complete -f -c fundle -n '__fish_prog_needs_command' -a version -d "display fundle version"
 complete -f -c fundle -n '__fish_prog_needs_command' -a help -d "display helps"
 
-complete -f -c fundle -n '__fish_prog_using_command install' -s u -l update -d "update existing plugins (deprecated)"
-
-complete -f -c fundle -n '__fish_prog_using_command list' -s s -l short -d "show a short list with plugin names only"
-
-complete -f -c fundle -n '__fish_prog_using_command init' -s p -l profile -d "profile time for loading each plugin"
+complete -f -c fundle -n '__fish_prog_using_command global-plugin' -l url -d "set the plugin URL"
+complete -f -c fundle -n '__fish_prog_using_command global-plugin' -l path -d "set the plugin load path"
 
 complete -f -c fundle -n '__fish_prog_using_command plugin' -l url -d "set the plugin URL"
 complete -f -c fundle -n '__fish_prog_using_command plugin' -l path -d "set the plugin load path"
