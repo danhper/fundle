fa2862d96098373e82e85ac989e7615e7e5e5322
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2022-10-28 16:51:47 -0500

    Optimize local installation script

--- install-fundle.fish
+++ install-fundle.fish
@@ -1,5 +1,8 @@
-echo "[Downloading fundle ...]";
-test -z "$XDG_CONFIG_HOME"; and set XDG_CONFIG_HOME ~/.config;
-mkdir -p $XDG_CONFIG_HOME/fish/functions;
-curl -sfL https://git.io/fundle > $XDG_CONFIG_HOME/fish/functions/fundle.fish; and fish -c "fundle install"; and exec fish;
+buitlin printf '[Downloading fundle ...]'
+builtin test -z "$XDG_CONFIG_HOME";
+  and builtin set XDG_CONFIG_HOME ~/.config;
+command mkdir -p $XDG_CONFIG_HOME/fish/functions;
+command curl -sfL https://git.io/fundle > $XDG_CONFIG_HOME/fish/functions/fundle.fish;
+  and fish -c "fundle install";
+  and builtin exec fish;
 # leave the semicolons at the end of the lines, they are needed by eval
