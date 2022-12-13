a2d3e732956975c5f8bc5282b448357e4748cbed
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2022-12-13 10:44:02 -0600

    Add sudo to additional commands

--- install-fundle-global.fish
+++ install-fundle-global.fish
@@ -1,5 +1,6 @@
 builtin echo "[Downloading fundle ...]";
-command mkdir -p /root/.config/fish/functions /root/.config/fish/completions
+command sudo mkdir -p /root/.config/fish/functions /root/.config/fish/completions
+command sudo chmod a+r -R /root/.config/fish
 
 command grep fundle.fish /root/.config/fish/config.fish;
 	or command sudo echo "builtin source /root/.config/fish/functions/fundle.fish" >> /root/.config/fish/config.fish
@@ -7,7 +8,7 @@ command grep fundle.fish /root/.config/fish/config.fish;
 command sudo echo "for f in (command find /etc/fish -type l -name '*.fish' -printf '%d %p\n' | command sort | command cut -d' ' -f2-)\n	builtin source (command readlink -f \"$f\")\nend" >> /etc/fish/config.fish
 
 for i in functions completions
-	command mkdir -p /root/.config/fish/$i
+	command sudo mkdir -p /root/.config/fish/$i
 	command sudo curl -sfL https://raw.githubusercontent.com/tuvistavie/fundle/master/$i/fundle.fish > /root/.config/fish/$i/fundle.fish;
 end
 
